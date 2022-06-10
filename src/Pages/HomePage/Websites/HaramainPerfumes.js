0001aefee01d9c33249b29c59679ba610a267773
ashrafur22 <ashrafurrahman22@gmail.com>
2022-06-10 23:22:54 +0600

    last

--- src/Pages/HomePage/Websites/HaramainPerfumes.js
+++ src/Pages/HomePage/Websites/HaramainPerfumes.js
@@ -1,10 +1,78 @@
 import React from 'react';
+import firstPic from '../../../assets/haramain/papaya.png';
+import secondPic from '../../../assets/haramain/heading.png';
+import thirdPic from '../../../assets/haramain/singleItem.png';
+
 
 const HaramainPerfumes = () => {
     return (
-        <div className='min-h-screen'>
-            <h2>this is perfumes</h2>
-        </div>
+        <div className="min-h-screen">
+        <div class="carousel w-full">
+<div id="slide1" class="carousel-item relative w-full">
+<img src={firstPic} />
+<div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+  <a href="#slide4" class="btn btn-circle">❮</a> 
+  <a href="#slide2" class="btn btn-circle">❯</a>
+</div>
+</div> 
+<div id="slide2" class="carousel-item relative w-full">
+<img src={secondPic} />
+<div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+  <a href="#slide1" class="btn btn-circle">❮</a> 
+  <a href="#slide3" class="btn btn-circle">❯</a>
+</div>
+</div> 
+<div id="slide3" class="carousel-item relative w-full">
+<img src={thirdPic} />
+<div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+  <a href="#slide2" class="btn btn-circle">❮</a> 
+  <a href="#slide4" class="btn btn-circle">❯</a>
+</div>
+</div>
+</div>
+
+    <div className='min-h-screen'>
+      <div className='my-5 bg-base-200 rounded-xl p-5'>
+      <div className='flex justify-center gap-20'>
+           <div>
+
+         <h2 className="mt-5 mb-3 text-3xl font-semibold">Features</h2>
+           <ol className='list-disc ml-5 text-xl'>
+            <li>Product Stock Update or delete System</li>
+            <li>Categorization of Products in stocks</li>
+            <li>Firebase Authentication</li>
+            <li>Inventory/Product History</li>
+            <li>Add new Product System</li>
+        </ol>
+        
+           </div>
+           <div>
+           <h2 className="mt-5 mb-3 text-3xl font-semibold">Technology</h2>
+        <ol className='list-disc ml-5 text-xl'>
+            <li>React</li>
+            <li>Express.js</li>
+            <li>MongoDB</li>
+            <li>Firebase</li>
+            <li>Bootstrap</li>
+            </ol>
+           </div>
+
+           <div className='flex flex-col gap-10 py-5'>
+           <a href="https://al-haramain-perfumes-5bad9.web.app/" target='blank' className="w-full">
+          <button class="btn w-full btn-primary">Live Link</button>
+          </a>
+          <a href="https://github.com/ashrafurrahman22/al-haramain-website" target='blank' className="w-full">
+          <button class="btn w-full btn-primary">client-side-repo-Link</button>
+          </a>
+          <a href="https://github.com/ashrafurrahman22/al-haramain-website-server" target='blank' className="w-full">
+          <button class="btn w-full btn-primary">Server-side-repo-Link</button>
+          </a>
+           </div>
+       </div>
+      </div>
+    </div>
+
+    </div>
     );
 };
 
--- src/Pages/HomePage/Websites/Wild.js
+++ src/Pages/HomePage/Websites/Wild.js
@@ -1,9 +1,73 @@
 import React from 'react';
+import present from '../../../assets/wild/zebras.png';
+import past from '../../../assets/wild/hello.png';
+import future from '../../../assets/wild/Screenshot (81).png';
 
 const Wild = () => {
     return (
+        <div className="min-h-screen">
+            <div class="carousel w-full">
+  <div id="slide1" class="carousel-item relative w-full">
+    <img src={present} />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide4" class="btn btn-circle">❮</a> 
+      <a href="#slide2" class="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide2" class="carousel-item relative w-full">
+    <img src={past} />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide1" class="btn btn-circle">❮</a> 
+      <a href="#slide3" class="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide3" class="carousel-item relative w-full">
+    <img src={future} />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide2" class="btn btn-circle">❮</a> 
+      <a href="#slide4" class="btn btn-circle">❯</a>
+    </div>
+  </div>
+</div>
+
         <div className='min-h-screen'>
-            <h2>this is for wild</h2>
+          <div className='my-5 bg-base-200 rounded-xl p-5'>
+          <div className='flex justify-center gap-20'>
+               <div>
+
+             <h2 className="mt-5 mb-3 text-3xl font-semibold">Features</h2>
+               <ol className='list-disc ml-5 text-xl'>
+                <li>Wild-life-Photos with Details</li>
+                <li>Service Booking</li>
+                <li>Firebase Authentication</li>
+                <li>Form for Service Booking</li>
+                <li>Acbout Section</li>
+            </ol>
+            
+               </div>
+               <div>
+               <h2 className="mt-5 mb-3 text-3xl font-semibold">Technology</h2>
+            <ol className='list-disc ml-5 text-xl'>
+                <li>React</li>
+                <li>React-Router-Dom</li>
+                <li>React-BootStrap</li>
+                <li>API</li>
+                <li>Firebase</li>
+                </ol>
+               </div>
+
+               <div className='flex flex-col gap-10 py-5'>
+               <a href="https://wild-movements.web.app/" target='blank' className="w-full">
+              <button class="btn w-full btn-primary">Live Link</button>
+              </a>
+              <a href="https://github.com/ashrafurrahman22/wild-movements-website" target='blank' className="w-full">
+              <button class="btn w-full btn-primary">client-side-repo-Link</button>
+              </a>
+               </div>
+           </div>
+          </div>
+        </div>
+
         </div>
     );
 };
