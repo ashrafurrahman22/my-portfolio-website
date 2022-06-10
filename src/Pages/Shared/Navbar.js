e552d2e1d7de02e0f73685d2fb89f4f6e982b569
ashrafur22 <ashrafurrahman22@gmail.com>
2022-06-10 23:34:11 +0600

    last

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
 
       const menuItems = <>
         <li><Link to='/'>Home</Link></li>
-    <li><Nav.Link href="/home#projects">Projects</Nav.Link></li>
+    <li><a href="/home#projects">Projects</a></li>
         <li><Link to='/contact'>Contact</Link></li>
         <li><Link to='/blogs'>Blogs</Link></li>
         <li><Link to='/about'>About Me</Link></li>
